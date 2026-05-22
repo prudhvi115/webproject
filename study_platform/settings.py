2c72b35b57f9ab90ee2452e9f446520908c92a81
prudhvi <23r91a04j7@tkrec.ac.in>
2026-05-22 19:30:30 +0530

    Update Render config and project settings for deployment

--- study_platform/settings.py
+++ study_platform/settings.py
@@ -90,13 +90,18 @@ WSGI_APPLICATION = 'study_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = config('DATABASE_URL', default='')
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
-}
-# DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'))
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
